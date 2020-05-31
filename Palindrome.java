c391432beefa0fe7539d050ac0c92ee496498251
rahulkumarppps <46070167+rahulkumarppps@users.noreply.github.com>
2020-06-01 00:37:06 +0530

    Create Palindrome.java

---
+++ Palindrome.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Palindrome{
+
+	public static void main(String[] args) {
+		//System.out.println("Hello");
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+		StringBuffer sb = new StringBuffer(str.toLowerCase());
+		for (int i = 0; i < sb.length() ; i++) {
+			if(!Character.isLetter(sb.charAt(i))){
+				sb.deleteCharAt(i);
+				i--;
+			}
+		}
+		str = sb.toString();
+		System.out.println(sb);
+		sb.reverse();
+		System.out.println(sb);
+		System.out.println(str.toLowerCase().equals(sb.toString()));
+	}
+}
